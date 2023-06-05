bf128611b293c2093b554ff80e123e7933dcc626
abdalrahman-droubi <abdalrahman.drobi@outlook.com>
2023-06-05 11:23:50 +0300

    new

--- routes/RoomDetails.js
+++ routes/RoomDetails.js
@@ -4,7 +4,7 @@ const pool = require("../config/dbConfig");
 
 router.get("/", async (req, res) => {
   try {
-    const allRooms = await pool.query("SELECT * FROM public.roominfo");
+    const allRooms = await pool.query("SELECT * FROM roominfo");
     res.json(allRooms.rows);
   } catch (error) {
     console.error(error.message);
@@ -13,13 +13,10 @@ router.get("/", async (req, res) => {
 
 router.post("/", async function (req, res) {
   try {
-    const room_type = req.body.roomType;
-    const number_of_room = req.body.roomNumber;
-    const room_img = req.body.roomImage;
-    const price = req.body.price;
+  const{room_type,number_of_room,price,number_of_beds,floor_area,descriptions,room_img,number_of_guests}=req.body
     const all_records = await pool.query(
-      "INSERT INTO roominfo (room_type, number_of_room, room_img, price ) VALUES($1, $2, $3 , $4) RETURNING *",
-      [room_type, number_of_room, room_img, price]
+      "INSERT INTO roominfo (room_type,number_of_room,price,number_of_beds,floor_area,descriptions,room_img,number_of_guests,hotel_id) VALUES($1, $2, $3 , $4,$5,$6,$7,$8,$9) RETURNING *",
+      [room_type,number_of_room,price,number_of_beds,floor_area,descriptions,room_img,number_of_guests]
     );
     res.json(all_records.rows);
   } catch (err) {
@@ -44,10 +41,10 @@ router.get("/:id", async (req, res) => {
 router.put("/:id", async (req, res) => {
     try {
       const { id } = req.params;
-      const { room_type, number_of_room, price, room_img } = req.body;
+      const {room_type,number_of_room,price,number_of_beds,floor_area,descriptions,room_img,number_of_guests} = req.body;
       const updateRoom = await pool.query(
-        "UPDATE roominfo SET room_type=$1,number_of_room=$2,price=$3,room_img=$4 WHERE room_id=$5",
-        [room_type, number_of_room, price, room_img, id]
+        "UPDATE roominfo SET room_type=$1,number_of_room=$2,price=$3,number_of_beds=$4,floor_area=$5,descriptions=$6,room_img=$7,number_of_guests=$8 WHERE room_id=$9",
+        [room_type,number_of_room,price,number_of_beds,floor_area,descriptions,room_img,number_of_guests,id]
       );
       res.json("hotels WAS UPDATING ");
     } catch (error) {
@@ -57,7 +54,7 @@ router.put("/:id", async (req, res) => {
   
   router.delete("/:id", async (req, res) => {
     try {
-      const { id } = req.params;
+      const {id} = req.params;
       const deleteRoom = await pool.query(
         "DELETE FROM roominfo Where room_id=$1",
         [id]
